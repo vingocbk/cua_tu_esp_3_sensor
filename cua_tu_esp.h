35e0f364340cd51ffa2bf0271875bcc6fdc62d49
vingocbk <vi.ngoc.bk@gmail.com>
2019-02-20 16:08:28 +0700

    finish all option

--- cua_tu_esp.h
+++ cua_tu_esp.h
@@ -2,6 +2,7 @@
 #include "AppDebug.h"
 #include "ESP8266WiFi.h"
 #include "ESP8266WebServer.h"
+#include "ESP8266HTTPClient.h"
 #include "ESP8266mDNS.h"
 #include "ArduinoJson.h"
 #include "Ticker.h"
@@ -39,18 +40,19 @@
 
 #define HTTP_PORT 80
 
-#define SSID "HPT 2B"
-#define PASS "hpt12345"
+// #define SSID "HPT 2B"
+// #define PASS "hpt12345"
 
 //#define SSID "hpt cnc"
 //#define PASS "hpt12345"
 #define CONFIG_HOLD_TIME 3000
 
+HTTPClient httpclient;
 ESP8266WebServer server(HTTP_PORT);
 
-IPAddress ip(10, 10, 9, 158); // where xx is the desired IP Address
-IPAddress gateway(10, 10, 9, 1); // set gateway to match your network
-IPAddress subnet(255, 255, 255, 0); // set subnet mask to match your network
+// IPAddress ip(10, 10, 9, 160); // where xx is the desired IP Address
+// IPAddress gateway(10, 10, 9, 1); // set gateway to match your network
+// IPAddress subnet(255, 255, 255, 0); // set subnet mask to match your network
 
 //IPAddress ip(3, 3, 0, 157); // where xx is the desired IP Address
 //IPAddress gateway(3, 3, 0, 1); // set gateway to match your network
@@ -77,17 +79,25 @@ bool fristRun = true;   //kiem tra lan chay dau tinh khoang cach chieu dai tu
 bool daytay = true;
 bool Flag_Normal_Mode = true;
 bool isSaveDistant = false;
+bool flag_send_status_when_use_hand = false;
 int start_count_hall_sensor = 0;    //bat dau den 2 thi moi tinh day tay
 int count_number_of_click = 0;      //click 5 lan thi chuyen mode
 uint8_t countFrirstRun = 0;  //dem so lan va cham
 uint32_t count_stop_motor = 0;
 uint32_t time_click_button = 0;
 uint32_t pre_time_click_button = 0;
+uint8_t first_octet;
+uint8_t second_octet;
+uint8_t third_octet;
+uint8_t fourth_octet;
+
+String esid, epass, eip;
 int stop_dau = 0;           //gia tri set cho khoang dung lai
 int stop_cuoi = 0;
 int loai_bien_giong_nhau_cua_cam_bien = 0;
 int luu_trang_thai_cua_sensor_ngay_khi_dung_lai = 0;
 
+//normal mode
 void handleOk();
 void handleRoot();
 void getStatus();
@@ -99,7 +109,7 @@ void dirhallSensor3();
 void setpwmMotor();
 void tickerupdate();
 void caculateSpeed();
-bool testWifi();
+bool testWifi(String esid, String epass);
 void StartNormalSever();
 void buttonClick();
 void Open();
@@ -110,10 +120,20 @@ void Stop();
 void StopClick();
 void setpwmStopMotor();
 void inputDistant();        //doc quang duong
-void setLedApMode();
-void setupConfigMode();
-String GetFullSSID();
 void resetDistant();
+void SetupNomalMode();
+void SetupNetwork();
+
+//config mode
+void setLedApMode();
+void SetupConfigMode();             //phat wifi
+void ConfigMode();                  //nhan data tu app
+void StartConfigServer();           //thiet lap sever
+bool connectToWifi(String nssid, String npass, String ip);
+String GetFullSSID();
+void detachIP(String ip);
+void setupIP();
+
 
 
 //Ticker tickerSetApMode(setLedApMode, 200, 0);   //every 200ms
--- cua_tu_esp_3_sensor.ino
+++ cua_tu_esp_3_sensor.ino
@@ -257,8 +257,10 @@ void caculateSpeed(){
 
         if(fristRun == false && countPulFGDistant <= 3){
             countPulFGDistant = 0;
+            flag_send_status_when_use_hand = true;
         }else if(fristRun == false && (countPulDistant -3) <= countPulFGDistant){
             countPulFGDistant = countPulDistant;
+            flag_send_status_when_use_hand = true;
         }
 
         
@@ -300,6 +302,48 @@ void setpwmStopMotor(){
         count_stop_motor = 0;
         statusStop = true;
         daytay = true;
+        if(flag_send_status_when_use_hand == true && Forward == true){      //dang o trang thai dong cua
+            httpclient.begin("http://10.10.9.110:8888/close"); //HTTP
+            int httpCode = httpclient.GET();
+            // httpCode will be negative on error
+            if (httpCode > 0) {
+                // HTTP header has been send and Server response header has been handled
+                ECHO("[HTTP] GET... code: ");
+                ECHOLN(httpCode);
+
+                // file found at server
+                if (httpCode == HTTP_CODE_OK) {
+                    String payload = httpclient.getString();
+                    ECHOLN(payload);
+                }
+            } else {
+                ECHO("[HTTP] GET... failed, error: ");
+                ECHOLN(httpclient.errorToString(httpCode).c_str());
+            }
+            httpclient.end();
+            flag_send_status_when_use_hand = false;
+        }
+        else if(flag_send_status_when_use_hand == true && Forward == false){    //dang o trang thai mo cua
+            httpclient.begin("http://10.10.9.110:8888/open"); //HTTP
+            int httpCode = httpclient.GET();
+            // httpCode will be negative on error
+            if (httpCode > 0) {
+                // HTTP header has been send and Server response header has been handled
+                ECHO("[HTTP] GET... code: ");
+                ECHOLN(httpCode);
+
+                // file found at server
+                if (httpCode == HTTP_CODE_OK) {
+                    String payload = httpclient.getString();
+                    ECHOLN(payload);
+                }
+            } else {
+                ECHO("[HTTP] GET... failed, error: ");
+                ECHOLN(httpclient.errorToString(httpCode).c_str());
+            }
+            httpclient.end();
+            flag_send_status_when_use_hand = false;
+        }
     }
     
 }
@@ -355,17 +399,50 @@ void tickerupdate(){
 }
 
 
-bool testWifi() {
+void SetupNomalMode(){
+    SetupNetwork();
+    if (WiFi.status() == WL_CONNECTED){
+        StartNormalSever();
+    }    
+}
+
+void SetupNetwork() {
+    ECHOLN("Reading EEPROM ssid");
+    esid = "";
+    for (int i = EEPROM_WIFI_SSID_START; i < EEPROM_WIFI_SSID_END; ++i){
+        esid += char(EEPROM.read(i));
+    }
+    ECHO("SSID: ");
+    ECHOLN(esid);
+    ECHOLN("Reading EEPROM pass");
+    epass = "";
+    for (int i = EEPROM_WIFI_PASS_START; i < EEPROM_WIFI_PASS_END; ++i){
+        epass += char(EEPROM.read(i));
+    }
+    ECHO("PASS: ");
+    ECHOLN(epass);
+    ECHOLN("Reading EEPROM IP");
+    eip = "";
+    for (int i = EEPROM_WIFI_IP_START; i < EEPROM_WIFI_IP_END; ++i){
+        eip += char(EEPROM.read(i));
+    }
+    ECHO("IP: ");
+    ECHOLN(eip);
+    detachIP(eip);  //tach ip thanh 4 kieu uint8_t
+    testWifi(esid, epass);
+}
+
+bool testWifi(String esid, String epass) {
     ECHO("Connecting to: ");
-    ECHOLN(SSID);
+    ECHOLN(esid);
     WiFi.softAPdisconnect();
     WiFi.disconnect();
     server.close();
     delay(1000);
-    //setupIP();      //cai dat ip theo quy dinh
-    WiFi.config(ip, gateway, subnet);
+    setupIP();      //cai dat ip theo quy dinh
+    // WiFi.config(ip, gateway, subnet);
     WiFi.mode(WIFI_STA);        //bat che do station
-    WiFi.begin(SSID, PASS);
+    WiFi.begin(esid.c_str(), epass.c_str());
     int c = 0;
     ECHOLN("Waiting for Wifi to connect");
     while (c < 20) {
@@ -379,9 +456,9 @@ bool testWifi() {
         delay(500);
         ECHO(".");
         c++;
-        // if(digitalRead(PIN_CONFIG) == LOW){
-        //     break;
-        // }
+        if(digitalRead(BUTTON) == HIGH){
+            break;
+        }
     }
     ECHOLN("");
     ECHOLN("Connect timed out");
@@ -405,6 +482,7 @@ void StartNormalSever(){
     server.on("/resetdistant", HTTP_OPTIONS, handleOk);
     server.begin();
     ECHOLN("HTTP server started");
+    
 }
 
 String GetFullSSID() {
@@ -421,7 +499,50 @@ String GetFullSSID() {
 }
 
 
-void setupConfigMode(){
+bool connectToWifi(String nssid, String npass, String ip) {
+    ECHOLN("Open STA....");
+    WiFi.softAPdisconnect();
+    WiFi.disconnect();
+    WiFi.mode(WIFI_STA);
+    delay(100);
+    setupIP();
+    //WiFi.begin(nssid.c_str(), npass.c_str());
+
+    if (testWifi(nssid, npass)) {
+        ECHOLN("clearing eeprom");
+        for (int i = 0; i < EEPROM_WIFI_MAX_CLEAR; ++i){ 
+            EEPROM.write(i, 0); 
+        }
+        ECHOLN("writing eeprom ssid:");
+        ECHO("Wrote: ");
+        for (int i = 0; i < nssid.length(); ++i){
+            EEPROM.write(i+EEPROM_WIFI_SSID_START, nssid[i]);             
+            ECHO(nssid[i]);
+        }
+        ECHOLN("");
+        ECHOLN("writing eeprom pass:"); 
+        ECHO("Wrote: ");
+        for (int i = 0; i < npass.length(); ++i){
+            EEPROM.write(i+EEPROM_WIFI_PASS_START, npass[i]);
+            ECHO(npass[i]);
+        }
+        ECHOLN("");
+        ECHOLN("writing eeprom IP:"); 
+        ECHO("Wrote: ");
+        for (int i = 0; i < ip.length(); ++i){
+            EEPROM.write(i+EEPROM_WIFI_IP_START, ip[i]);             
+            ECHO(ip[i]);
+        }
+        ECHOLN("");
+        EEPROM.commit();
+        ECHOLN("Done writing!");
+        return true;
+    }
+
+    return false;
+}
+
+void SetupConfigMode(){
     ECHOLN("[WifiService][setupAP] Open AP....");
     WiFi.softAPdisconnect();
     WiFi.disconnect();
@@ -443,6 +564,105 @@ void setupConfigMode(){
 }
 
 
+void StartConfigServer(){    
+    ECHOLN("[HttpServerH][startConfigServer] Begin create new server...");
+//    server = new ESP8266WebServer(HTTP_PORT);
+    server.on("/", HTTP_GET, handleRoot);
+    // server.on("/cleareeprom", HTTP_GET, clearEeprom);
+    server.on("/config", HTTP_POST, ConfigMode);
+    server.on("/", HTTP_OPTIONS, handleOk);
+    // server.on("/cleareeprom", HTTP_OPTIONS, handleOk);
+    server.on("/config", HTTP_OPTIONS, handleOk);
+    server.begin();
+    ECHOLN("[HttpServerH][startConfigServer] HTTP server started");
+}
+
+
+void ConfigMode(){
+    StaticJsonBuffer<RESPONSE_LENGTH> jsonBuffer;
+    ECHOLN(server.arg("plain"));
+    JsonObject& rootData = jsonBuffer.parseObject(server.arg("plain"));
+    ECHOLN("--------------");
+    tickerSetApMode.stop();
+    if (rootData.success()) {
+        server.sendHeader("Access-Control-Allow-Headers", "*");
+        server.sendHeader("Access-Control-Allow-Origin", "*");
+        server.send(200, "application/json; charset=utf-8", "{\"status\":\"success\"}");
+        //server.stop();
+        String nssid = rootData["ssid"];
+        String npass = rootData["password"];
+        String ip = rootData["set_ip"];
+
+        detachIP(ip); 
+        ECHO("Wifi new name: ");
+        ECHOLN(nssid);
+        ECHO("Wifi new password: ");
+        ECHOLN(npass);
+        ECHO("Wifi new IP: ");
+        ECHO(first_octet);
+        ECHO(".");
+        ECHO(second_octet);
+        ECHO(".");
+        ECHO(third_octet);
+        ECHO(".");
+        ECHOLN(fourth_octet);
+        if (connectToWifi(nssid, npass, ip)) {
+            esid = nssid;
+            epass = npass;
+            StartNormalSever();
+            Flag_Normal_Mode = true;
+            return;
+        }
+        tickerSetApMode.start();
+        ECHOLN("Wrong wifi!!!");
+        SetupConfigMode();
+        StartConfigServer();
+        return;
+    }
+    ECHOLN("Wrong data!!!");
+}
+
+void detachIP(String ip){
+    String first_octetStr = "";
+    String second_octetStr = "";
+    String third_octetStr = "";
+    String fourth_octetStr = "";
+    for(int i = 0, j = 0; i <= sizeof(ip)+1; i++){
+        char c = ip[i];
+        if(c == '.'){
+            j++;
+            continue;
+        }
+        switch(j){
+            case 0:
+                first_octetStr += c;
+                break;
+            case 1:
+                second_octetStr += c;
+                break;
+            case 2:
+                third_octetStr += c;
+                break;
+            case 3:
+                fourth_octetStr += c;
+                break;
+        }
+
+    }
+
+    first_octet = atoi(first_octetStr.c_str());
+    second_octet = atoi(second_octetStr.c_str());
+    third_octet = atoi(third_octetStr.c_str());
+    fourth_octet = atoi(fourth_octetStr.c_str());
+}
+
+void setupIP(){
+    // config static IP
+    IPAddress ip(first_octet, second_octet, third_octet, fourth_octet); // where xx is the desired IP Address
+    IPAddress gateway(first_octet, second_octet, third_octet, 1); // set gateway to match your network
+    IPAddress subnet(255, 255, 255, 0); // set subnet mask to match your network
+    WiFi.config(ip, gateway, subnet);
+}
 
 
 void setup() {
@@ -470,9 +690,11 @@ void setup() {
         ECHO("read EEPROM done: ");
     }
 
+    SetupNomalMode();
 
-    testWifi();
-    StartNormalSever();
+    // testWifi();
+    // StartNormalSever();
+
 
     attachInterrupt(digitalPinToInterrupt(hallSensor1), dirhallSensor1, RISING);
     attachInterrupt(digitalPinToInterrupt(hallSensor2), dirhallSensor2, RISING);
@@ -480,7 +702,7 @@ void setup() {
     attachInterrupt(digitalPinToInterrupt(hallSensor3), dirhallSensor3, RISING);
     // attachInterrupt(digitalPinToInterrupt(BUTTON), buttonClick, RISING);
 
-    if(EEPROM.read(EEPROM_DISTANT) != 255){
+    if(EEPROM.read(EEPROM_DISTANT) != 255 && EEPROM.read(EEPROM_DISTANT) != 0){
         isSaveDistant = true;
         countPulDistant = EEPROM.read(EEPROM_DISTANT);
         ECHO("Distant = ");
@@ -511,24 +733,27 @@ void loop() {
 
     if (Flag_Normal_Mode == true && WiFi.status() != WL_CONNECTED){
         digitalWrite(ledTestWifi, HIGH);
-        if (testWifi()){
+        if (testWifi(esid, epass)){
             StartNormalSever();
         }
     }
 
+    //click button to control
     if(Flag_Normal_Mode == true && digitalRead(BUTTON) == HIGH){
         buttonClick();
     }else{
         time_click_button = millis();
     }
-
+    
+    //hold to config mode
     if(digitalRead(BUTTON) == HIGH && (time_click_button + CONFIG_HOLD_TIME) <= millis()){
         time_click_button = millis();
         StopClick();
         digitalWrite(ledTestWifi, HIGH);
         Flag_Normal_Mode = false;
         tickerSetApMode.start();
-        setupConfigMode();
+        SetupConfigMode();
+        StartConfigServer();
     }
 
 
